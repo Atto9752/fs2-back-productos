4d555bfb00ac513624d3fc114dd678f5aabdbeec
Atto9752 <anto.faundes@duocuc.cl>
2025-12-03 16:16:20 -0300

    avance en controller

--- productos/src/main/java/com/example/stroms/productos/controller/productoControlador.java
+++ productos/src/main/java/com/example/stroms/productos/controller/productoControlador.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import com.example.stroms.productos.model.dto.producto;
 import com.example.stroms.productos.service.productoService;
 
@@ -17,22 +20,27 @@ public class productoControlador {
     @Autowired
     private productoService servicio;
 
+    @GetMapping
     public List<producto> listarProductos() {
         return servicio.listarProductos();
     }
 
+    @PostMapping
     public producto guardarProducto(producto producto) {
         return servicio.guardarProducto(producto);
     }
 
+    @GetMapping("/{id}")
     public producto buscarProducto(int id) {
         return servicio.buscarProducto(id);
     }
 
+    @DeleteMapping("/{id}")
     public void eliminarProducto(int id) {
         servicio.eliminarProducto(id);
     }
 
+    @PutMapping("/{id}")
     public producto actualizarProducto(int id, producto productoActualizado) {
         return servicio.actualizarProducto(id, productoActualizado);
     }
