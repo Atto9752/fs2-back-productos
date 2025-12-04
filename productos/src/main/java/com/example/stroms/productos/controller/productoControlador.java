baad38560372762d5364ff43f1109e4237a89221
Atto9752 <anto.faundes@duocuc.cl>
2025-12-04 12:29:37 -0300

    correccion
    correcciones en controller para que la estructura REST sea adecuada a @RequestParam

--- productos/src/main/java/com/example/stroms/productos/controller/productoControlador.java
+++ productos/src/main/java/com/example/stroms/productos/controller/productoControlador.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,17 +34,17 @@ public class productoControlador {
         return servicio.guardarProducto(producto);
     }
 
-    @GetMapping("/search/{id}")
+    @GetMapping("/search")
     public producto buscarProducto(@RequestParam int id) {
         return servicio.buscarProducto(id);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/delete")
     public void eliminarProducto(@RequestParam int id) {
         servicio.eliminarProducto(id);
     }
 
-    @PutMapping("/update/{id}")
+    @PutMapping("/update")
     public producto actualizarProducto(@RequestParam int id, @RequestBody producto productoActualizado) {
         return servicio.actualizarProducto(id, productoActualizado);
     }
