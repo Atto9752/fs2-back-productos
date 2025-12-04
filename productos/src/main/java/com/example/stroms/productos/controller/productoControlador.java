d4510009a53512f17c464483f1d9fb251504814c
Atto9752 <anto.faundes@duocuc.cl>
2025-12-04 05:18:16 -0300

    correcciones varias

--- productos/src/main/java/com/example/stroms/productos/controller/productoControlador.java
+++ productos/src/main/java/com/example/stroms/productos/controller/productoControlador.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import com.example.stroms.productos.model.dto.producto;
 import com.example.stroms.productos.service.productoService;
 
@@ -20,28 +25,28 @@ public class productoControlador {
     @Autowired
     private productoService servicio;
 
-    @GetMapping
+    @GetMapping("/all")
     public List<producto> listarProductos() {
         return servicio.listarProductos();
     }
 
-    @PostMapping
-    public producto guardarProducto(producto producto) {
+    @PostMapping("/add")
+    public producto guardarProducto(@RequestBody producto producto) {
         return servicio.guardarProducto(producto);
     }
 
-    @GetMapping("/{id}")
-    public producto buscarProducto(int id) {
+    @GetMapping("/search/{id}")
+    public producto buscarProducto(@RequestParam int id) {
         return servicio.buscarProducto(id);
     }
 
-    @DeleteMapping("/{id}")
-    public void eliminarProducto(int id) {
+    @DeleteMapping("/delete/{id}")
+    public void eliminarProducto(@RequestParam int id) {
         servicio.eliminarProducto(id);
     }
 
-    @PutMapping("/{id}")
-    public producto actualizarProducto(int id, producto productoActualizado) {
+    @PutMapping("/update/{id}")
+    public producto actualizarProducto(@RequestParam int id, @RequestBody producto productoActualizado) {
         return servicio.actualizarProducto(id, productoActualizado);
     }
 }
